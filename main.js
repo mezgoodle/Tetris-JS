1944942030f18abf77b896a447531d4715d13e99
mezgoodle <mezgoodle@gmail.com>
2020-03-28 21:40:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ const
 // Calculate size of canvas from constants.
 ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+// Scale blocks
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
