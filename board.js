a3e2c2613bcaf2c9cdd1106229a6b45fffc2eb06
mezgoodle <mezgoodle@gmail.com>
2020-03-28 21:47:17 +0200

    Update board.js

---
+++ board.js
@@ -0,0 +1,13 @@
+class Board {
+    grid;
+
+    // Reset the board when we start a new game.
+    reset() {
+        this.grid = this.getEmptyBoard();
+    }
+
+    // Get matrix filled with zeros.
+    getEmptyBoard() {
+        return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+    }
+}
