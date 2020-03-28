2167b323f4c8a171ecd98d72d59d16a0265a75bc
mezgoodle <mezgoodle@gmail.com>
2020-03-28 21:40:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const
  canvas = document.getElementById('board'),
  ctx = canvas.getContext('2d');
+
+// Calculate size of canvas from constants.
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
