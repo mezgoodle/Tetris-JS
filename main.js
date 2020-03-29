693dfcb13612ad8c8049ac0675473d3573e94a21
mezgoodle <mezgoodle@gmail.com>
2020-03-29 12:16:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,26 @@ moves = {
     [KEY.UP]: p => ({...p, y: p.y + 1 })
 };
 
+document.addEventListener('keydown', event => {
+    if (moves[event.keyCode]) {
+        // Stop the event from bubbling.
+        event.preventDefault();
+
+        // Get new state of piece
+        let p = moves[event.keyCode](board.piece);
+
+        if (board.valid(p)) {
+            // If the move is valid, move the piece.
+            board.piece.move(p);
+
+            // Clear old position before drawing.
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+            board.piece.draw();
+        }
+    }
+});
+
 let board = new Board();
 
 function play() {
